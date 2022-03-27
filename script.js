eeb37ee1089be84832cec898270e274577af215f
mahil_dudhat <71947376+mahil-dudhat@users.noreply.github.com>
2022-03-27 20:06:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const scene = new THREE.Scene()
 const canvas = document.querySelector('.webgl')
 console.log(canvas)
 
-const geometry = new THREE.BoxGeometry(1,1,1)
+const geometry = new THREE.BoxGeometry(3,3,3)
+
 const material = new THREE.MeshBasicMaterial({color : 0xff0000})
+
+const axesHelper = new THREE.AxesHelper( 25 );
+scene.add( axesHelper );
+
 const mesh = new THREE.Mesh(geometry,material)
+
+const plane_geo = new THREE.PlaneGeometry( 250 , 100 )
+const plane_mate = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const plane = new THREE.Mesh( plane_geo, plane_mate );
+plane.position.z =  -50
+scene.add( plane );
+
 scene.add(mesh)
 
 const sizes = { 
@@ -24,10 +36,11 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
 })
 
-
+//const controls = new OrbitControls( camera, renderer.domElement )
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width  / sizes.height, 0.1 , 1000)
-camera.position.z = 3
+camera.position.z = 10
+
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
@@ -39,9 +52,31 @@ const clock = new THREE.Clock()
 
 const loop = () =>{
 
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()*2
+    
+    if (elapsedTime < 8)  {
+      mesh.position.x = -2 + elapsedTime
+      mesh.position.y = -2 + (elapsedTime/4)
+      mesh.position.z = 5 - elapsedTime
+    }
+
+    if (elapsedTime > 8 && elapsedTime<14)  {
+        mesh.position.x = 14 - elapsedTime 
+    }
+
+    if (elapsedTime > 5 && elapsedTime<10)  {
+        // mesh.position.x = -2 + elapsedTime 
+        // mesh.position.y = -2 + elapsedTime
+        // mesh.position.z = 5 - elapsedTime
+      }
+
+   
+     
 
     mesh.rotation.y = elapsedTime * Math.PI 
+
+     
+    
     
     renderer.render(scene,camera)
 
